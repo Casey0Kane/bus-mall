8edb3b86048f9574813379a69965e368b67880a8
Casey O'Kane <okanecasey@gmail.com>
2016-10-05 08:13:41 -0700

    working chart

--- app.js
+++ app.js
@@ -6,26 +6,27 @@ var selectionDisplay = document.getElementById('selectionDisplay');
 var choices = document.getElementById('choices');
 var results = document.getElementById('results');
 var complete = document.getElementById('complete');
+var chartData = document.getElementById('displayCharts');
 
 new BusMallItem('img/banana.jpg', 'Banana Slicer', 'banana');
-new BusMallItem('img/bathroom.jpg', 'Bathroom Device Stand', 'bathroom' );
-new BusMallItem('img/boots.jpg', 'Toeless Rain Boots', 'boots' );
+new BusMallItem('img/bathroom.jpg', 'Bathroom', 'bathroom' );
+new BusMallItem('img/boots.jpg', 'Rain Boots', 'boots' );
 new BusMallItem('img/breakfast.jpg', 'Toaster Pan Pot', 'breakfast' );
 new BusMallItem('img/bubblegum.jpg', 'Meatball BubbleGum', 'bubblegum' );
-new BusMallItem('img/chair.jpg', 'Uncomfortable Chair', 'chair' );
+new BusMallItem('img/chair.jpg', 'Chair', 'chair' );
 new BusMallItem('img/cthulhu.jpg', 'Cthulhu', 'cthulhu' );
-new BusMallItem('img/dog-duck.jpg', 'Dog Duck Beak', 'dog-duck' );
+new BusMallItem('img/dog-duck.jpg', 'Dog Duck', 'dog-duck' );
 new BusMallItem('img/dragon.jpg', 'Dragon Meat', 'dragon' );
 new BusMallItem('img/pen.jpg', 'Pen Cap Utensils', 'pen' );
-new BusMallItem('img/pet-sweep.jpg', 'Pet Sweep Slippers', 'pet-sweep' );
+new BusMallItem('img/pet-sweep.jpg', 'Pet Sweep', 'pet-sweep' );
 new BusMallItem('img/scissors.jpg', 'Pizza Scissors', 'scissors' );
-new BusMallItem('img/shark.jpg', 'Shark Sleeping Bag', 'shark' );
-new BusMallItem('img/sweep.jpg', 'Baby Sweeper', 'sweep' );
-new BusMallItem('img/tauntaun.jpg', 'Tauntaun Sleeping Bag', 'tauntaun' );
+new BusMallItem('img/shark.jpg', 'Shark', 'shark' );
+new BusMallItem('img/sweep.jpg', 'Sweeper', 'sweep' );
+new BusMallItem('img/tauntaun.jpg', 'Tauntaun', 'tauntaun' );
 new BusMallItem('img/unicorn.jpg', 'Unicorn Meat', 'unicorn' );
-new BusMallItem('img/usb.jpg', 'Tentical USB', 'usb' );
-new BusMallItem('img/water-can.jpg', 'Self Filling Water Can', 'water-can' );
-new BusMallItem('img/wine-glass.jpg', 'Useless Wineglass', 'wine-glass' );
+new BusMallItem('img/usb.jpg', 'USB', 'usb' );
+new BusMallItem('img/water-can.jpg', 'Water Can', 'water-can' );
+new BusMallItem('img/wine-glass.jpg', 'Wine glass', 'wine-glass' );
 console.log(itemsArray);
 
 function BusMallItem(imgSrc, displayName, name){
@@ -65,7 +66,7 @@ function getThreeItems(){
       item.timesDisplayed += 1;
     }
     else {
-      console.log('Did not add ' + item.name);
+      console.log('tried to duplicate ' + item.name);
       validItem = true;
     }
   }
@@ -104,8 +105,8 @@ function runMain(){
 
 function handleClickEvent(event){
   if(event.target.alt === undefined){//eslint-disable-line
-    console.log('Invalid Click Response');
-    return alert('You must Click on an Image!');
+    console.log('Invalid click response');
+    return alert('You must click on an image!');
   }
 
   var choice = event.target.alt;
@@ -134,13 +135,41 @@ function displayResults(){
     lineElement.textContent = itemsArray[i].name + ': Clicked/Displayed - ' + itemsArray[i].timesClicked + '/' + itemsArray[i].timesDisplayed;
     results.appendChild(lineElement);
   }
+  prepareLabelsAndData();
+  drawChart();
 }
 
 selectionDisplay.addEventListener('click', handleClickEvent);
 complete.addEventListener('click',displayResults);
+var labelArray = [];
+var dataArray = [];
+function prepareLabelsAndData(){
+  for (var i = 0; i < itemsArray.length; i++){
+    labelArray[i] = itemsArray[i].name;
+    labelArray[i] = labelArray[i].charAt(0).toUpperCase() + labelArray[i].slice(1);
+    dataArray[i] = itemsArray[i].timesClicked;
+  }
+  console.log(labelArray + ' ' + dataArray);
+}
 
+var data = {
+  labels: labelArray,
+  datasets: [
+    {
+      data: dataArray,
+      backgroundColor: [
+        'cyan', 'black', 'cyan', 'black', 'cyan', 'black', 'cyan', 'black', 'cyan', 'black', 'cyan', 'black', 'cyan', 'black', 'cyan', 'black', 'cyan', 'black', 'cyan'
+      ],
+    }]
+};
+
+function drawChart() {
+  var voteChart = new Chart(chartData,{
+    type: 'bar',
+    data: data,
+    options: {responsive: false},
+    scales: [{ticks:{ beginAtZero:true}}] });
+  console.log(voteChart);
+}
 
 runMain();
-
-var canvas = document.getElementById('myCanvas');
-var ctx = canvas.getContext('2d');
